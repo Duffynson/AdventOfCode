e8f176b3d1f06e815570f121068c315c5070837e
Duffynson <jan.dufek666@gmail.com>
2024-12-16 17:47:33 +0100

    day 12 part 2 finished

--- AOC2024/AOC2024/day12.cpp
+++ AOC2024/AOC2024/day12.cpp
@@ -32,22 +32,15 @@ bool isInPlot(const std::set<std::pair<int, int>>& plot, std::pair<int,int> poin
 int countPlotCorners(const std::set<std::pair<int, int>>& plot) {
     int result = 0;
     for (const auto& [x, y] : plot) {
-        bool topLeft = isInPlot(plot, { x - 1, y - 1 });
-        bool topMid = isInPlot(plot, { x - 1, y });
-        bool topRight = isInPlot(plot, { x - 1, y + 1 });
-        bool midLeft = isInPlot(plot, { x, y - 1 });
-        bool midRight = isInPlot(plot, { x, y + 1 });
-        bool botLeft = isInPlot(plot, { x + 1, y - 1 });
-        bool botMid = isInPlot(plot, { x + 1, y});
-        bool botRight = isInPlot(plot, { x + 1, y + 1 });
-        if (midLeft && botMid && !topRight) result++;
-        if (midRight && topMid && !botLeft) result++;
-        if (midRight && botMid && !topLeft) result++;
-        if (midLeft && topMid && !botRight) result++;
-        if (topMid && midRight && !topRight) result++;
-        if (topMid && midLeft && !topLeft) result++;
-        if (botMid && midRight && !botRight) result++;
-        if (botMid && midLeft && !botLeft) result++;
+        int neighbors = 0;
+        for (const auto& [dirX, dirY] : diagonalDirections) {
+            if (isInPlot(plot, { x + dirX, y + dirY })) {
+                neighbors++;
+            }
+        }
+        if (neighbors == 3 || neighbors == 7 || neighbors == 4) {
+            result++;
+        }
     }
     return result;
 }
@@ -131,67 +124,6 @@ void Day12::Task2() const {
         std::set<std::pair<int, int>> maximizedPlot = maximizePlot(currentPlot, 2);
         int x = countPlotCorners(maximizedPlot);
         result += x * plot.size();
-    }
-    std::vector<std::set<std::pair<int, int>>> fences;
-    for (int i = 0; i < allPlots.size(); i++) {
-        std::set<std::pair<int, int>> fence;
-        for (auto [x, y] : allPlots[i]) {
-            for (const auto& dir : directions) {
-                int newRow = x + dir.first;
-                int newCol = y + dir.second;
-                if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols) {
-                    fence.insert({newRow, newCol});
-                    continue;
-                }
-                if (input[x][y] == input[newRow][newCol]) {
-                    continue;
-                }
-                fence.insert({ newRow, newCol });
-            }
-        }
-        fences.push_back(fence);
-    }
-
-
-    for (int i = 0; i < fences.size(); i++) {
-        std::pair<int, int> direction = { 0,1 };
-        auto fence = fences[i];
-        auto firstFence = *fence.begin();
-        auto previousFence = firstFence;
-        std::set<std::pair<int, int>> seen;
-        seen.insert(firstFence);
-        int sides = 0;
-        while (true) {
-            std::pair<int, int> nextFence = {previousFence.first + direction.first, previousFence.second + direction.second};
-            if (firstFence.first == nextFence.first && firstFence.second == nextFence.second) {
-                sides++;
-                break;
-            }
-            if (fence.find(nextFence) != fence.end()) {
-                seen.insert(nextFence);
-                previousFence = nextFence;
-                continue;
-            }
-            else {
-                for (auto [x, y] : directions) {
-                    if (direction.first == x && direction.second == y) {
-                        continue;
-                    }
-                    nextFence = { previousFence.first + x, previousFence.second + y };
-                    if (seen.find(nextFence) != seen.end()) {
-                        continue;
-                    }
-                    if (fence.find(nextFence) != fence.end()) {
-                        seen.insert(nextFence);
-                        sides++;
-                        direction = { x,y };
-                        break;
-                    }
-                }
-            }
-            previousFence = nextFence;
-        }
-        result += sides * allPlots[i].size();
     }
 
     std::cout << result << std::endl;
