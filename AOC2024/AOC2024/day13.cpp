96e6d1ce78efa2f824d4d5a87328fd68e58dd35c
Duffynson <jan.dufek666@gmail.com>
2024-12-16 22:27:34 +0100

    day 13 prep #2

---
+++ AOC2024/AOC2024/day13.cpp
@@ -0,0 +1,10 @@
+#include "day13.h"
+
+void Day13::Task1() const {
+	auto input = ReadAllLinesInFile("input.txt");
+
+}
+
+void Day13::Task2() const {
+
+}
--- AOC2024/AOC2024/day13.h
+++ AOC2024/AOC2024/day13.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "day.h"
+#include "utils/parser.h"
+
+class Day13 : public Day {
+public:
+	Day13() {}
+	void Task1() const override;
+	void Task2() const override;
+};
